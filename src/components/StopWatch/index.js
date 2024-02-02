112d8e780104661b7089fb46c4e2e3e16634791c
Olha Shtepa <olgaolga2414@gmail.com>
2024-02-02 17:58:34 +0200

    add StopWatch

---
+++ src/components/StopWatch/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import styles from "./StopWatch.module.css/";
+class StopWatch extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {time:new Date(0,0,0,0,0,0)};
+        this.idInterval = null;
+    }
+    tick=()=>{
+        this.setState((state)=>{
+            const {time} = state;
+            const newTime = new Date(time.getTime()+1000);
+            return{time:newTime};
+        })
+    }
+    start = ()=>{
+        this.stop();
+        this.idInterval = setInterval(this.tick, 1000)
+    };
+
+    reset = ()=>{
+        this.stop();
+        this.setState({time:new Date(0,0,0,0,0,0,)})
+    };
+
+    stop = ()=>{
+        clearInterval(this.idInterval);
+        this.idInterval = null;
+    };
+
+    componentDidMount (){
+        this.start();
+    }
+    componentWillUnmount(){
+        this.stop();
+    }
+
+    render() {
+        const {time} = this.state;
+        return (
+            <artricle className={styles.container}>
+                <h2>{time.toLocaleTimeString('it-IT')}</h2>
+                <button onClick={this.start}>start</button>
+                <button onClick={this.reset}>reset</button>
+                <button onClick={this.stop}>stop</button>
+            </artricle>
+        );
+    }
+}
+
+export default StopWatch;
