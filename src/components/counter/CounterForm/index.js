7ba0160731e5b41e5655c71c56e90fa216d6095e
Olha Shtepa <olgaolga2414@gmail.com>
2024-02-07 19:30:52 +0200

    add handlerAdding&handierInput

--- src/components/counter/CounterForm/index.js
+++ src/components/counter/CounterForm/index.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import CounterSection from '../CounterSection';
 
 class CounterForm extends Component {
     constructor(props) {
         super(props);
-        this.state ={};
+        this.state ={step:1};
     }
     
+    handierInput = ({target:{name, value}})=>{
+        this.setState({[name]:Number(value)})
+    }
     render() {
+        const {counter} = this.state;
+        const {step} = this.props;
         return (
             <form>
-                <input type='number' />
+                <input
+                type='number' placeholder='step'
+                name='step'
+                value ={step} 
+                onClick={this.handierInput} 
+                min='1' max='1000000' step='1'
+                />
+                <CounterSection counter={counter}/>
             </form>
         );
     }
--- src/components/counter/CounterSection/index.js
+++ src/components/counter/CounterSection/index.js
@@ -9,16 +9,18 @@ class CounterSection extends Component {
         };
     }
     handlerAdding =()=>{
-
-    }
-    
-    
+        this.setState((state, props)=>({
+            counter:state.counter+props.state,  
+        } ) )
+       }
 
     render() {
         const {counter} = this.state;
+        const {step} = this.props;
         return (
             <div>
                 <h2>{counter}</h2>
+                <h2>{step}</h2>
                 <button onClick={this.handlerAdding}>Add</button> 
             </div>
         );
