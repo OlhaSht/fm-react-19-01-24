6f6ffe5d64e024812692bce238ee8750cb1ab081
Olha Shtepa <olgaolga2414@gmail.com>
2024-03-10 15:02:22 +0200

    use with date-fns&btn reset

--- src/components/FunkStopWatch/index.js
+++ src/components/FunkStopWatch/index.js
@@ -1,14 +1,20 @@
+//075/1 --лекция
 
 import React, { useState, useEffect } from 'react';
+import {format, addMilliseconds} from 'date-fns';
 
 const FunkStopWatch = () => {
-    const[time, setTime] = useState(0);
+    const[time, setTime] = useState(new Date(0,0,0,0,0,0));
     const [isRunning, setIsRunning]= useState(false);
     const handlerBtn  = () =>{setIsRunning(!isRunning)}
+    const handlerReset = ()=>{
+        setTime(new Date(0,0,0,0,0,0));
+        setIsRunning(false)
+    }
     useEffect (()=>{
        if(isRunning){
          const idInterval = setInterval(()=>{
-            setTime(time=>time+1)
+            setTime(time=> addMilliseconds(time, 1000))
          }, 1000);   
          return ()=>{
             clearInterval(idInterval)
@@ -18,8 +24,9 @@ const FunkStopWatch = () => {
     return (
         <div>
             <h2>Stop Watch</h2>
-            <h3>{time}</h3>
+            <h3>{format(time, 'HH:mm:ss')}</h3>
             <button onClick={handlerBtn}>{isRunning?'Stop':'Start'}</button>
+            <button onClick={handlerReset}>Reset</button>
         </div>
     );
 }
